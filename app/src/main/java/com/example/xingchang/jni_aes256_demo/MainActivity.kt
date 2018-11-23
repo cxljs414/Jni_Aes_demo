3618f17b0701734f9584ee480cb6ef9398cc9e68
xing.chang <xing.chang@7fresh.com>
2018-11-23 18:08:32 +0800

    java加解密

--- app/src/main/java/com/example/xingchang/jni_aes256_demo/MainActivity.kt
+++ app/src/main/java/com/example/xingchang/jni_aes256_demo/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.xingchang.jni_aes256_demo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start.setOnClickListener {
+            startJni()
+        }
+
+    }
+
+    private fun startJni() {
         //jni加解密
         val jniUtil = JniUtil()
-        val str= "abcdefg1234567"
+        val str= "b5e52565b81d171510dc0afd752b1d64"
         val encryptResult= jniUtil.encrypt(str)
         Log.i("jniUtil","JNI encryptResult=$encryptResult")
         val decryptResult= jniUtil.decrypt(encryptResult)
