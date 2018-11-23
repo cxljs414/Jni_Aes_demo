0e08bba2f723b41fa288ff61c543801543d6695b
xing.chang <xing.chang@7fresh.com>
2018-11-23 15:56:59 +0800

    java加解密

--- app/src/main/java/com/example/xingchang/jni_aes256_demo/MainActivity.kt
+++ app/src/main/java/com/example/xingchang/jni_aes256_demo/MainActivity.kt
@@ -9,11 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //jni加解密
         val jniUtil = JniUtil()
         val str= "abcdefg1234567"
         val encryptResult= jniUtil.encrypt(str)
-        Log.i("jniUtil","encryptResult=$encryptResult")
+        Log.i("jniUtil","JNI encryptResult=$encryptResult")
         val decryptResult= jniUtil.decrypt(encryptResult)
-        Log.i("jniUtil","decryptResult=$decryptResult")
+        Log.i("jniUtil","JNI decryptResult=$decryptResult")
+
+        //java加解密
+        val key_md5 ="b5e52765b81d101510dc0afdc52b1d64"
+        val javaEncrypt= AesUtils.aesEncrypt(str,key_md5)
+        Log.i("jniUtil","Java encryptResult=$javaEncrypt")
+        val javaDecrypt = AesUtils.aesDecrypt(javaEncrypt,key_md5)
+        Log.i("jniUtil","Java decryptResult=$javaDecrypt")
     }
 }
